d06d2238033ca0065a62bf5a67f57159e990433f
Nolat <NolatYT@gmail.com>
2021-05-04 10:31:47 +0200

    📦 Remove backend server package

--- backend/src/modules/hello/hello.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from "@nestjs/graphql";
-
-@Resolver(() => String)
-export class HelloResolver {
-  @Query(() => String)
-  hello(): string {
-    return "Hello world!";
-  }
-}

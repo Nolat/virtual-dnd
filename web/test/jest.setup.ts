d819105d751aa338baee8c7c38a7868143b422ab
Nolat <NolatYT@gmail.com>
2021-05-11 17:28:49 +0200

    Test config

--- web/test/jest.setup.ts
+++ web/test/jest.setup.ts
@@ -2,12 +2,32 @@ import "isomorphic-unfetch";
 
 import dotenv from "dotenv";
 import nock from "nock";
+import { Connection, createConnection } from "typeorm";
 
 dotenv.config({ path: ".env.test" });
 
-afterAll(() => {
+let connection: Connection;
+
+beforeAll(async (done) => {
+  connection = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: true,
+    logging: false,
+    cache: false,
+    dropSchema: true,
+    entities: ["../src/modules/api/models/**.model.ts"]
+  });
+
+  done();
+});
+
+afterAll((done) => {
   nock.cleanAll();
   nock.restore();
+  connection.close();
+
+  done();
 });
 
 window.matchMedia = jest.fn().mockImplementation((query) => {

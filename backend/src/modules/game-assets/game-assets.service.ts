819688d4fbd40d424dfd0f5349e366c29610b245
Nolat <NolatYT@gmail.com>
2021-05-29 08:57:26 +0200

    Fix image upload on gcloud

--- backend/src/modules/game-assets/game-assets.service.ts
+++ backend/src/modules/game-assets/game-assets.service.ts
@@ -2,12 +2,18 @@ import { Storage } from "@google-cloud/storage";
 import { Injectable } from "@nestjs/common";
 import { FileUpload } from "graphql-upload";
 
-const storage = new Storage({ keyFilename: "key.json" });
+const storage = new Storage({
+  keyFilename: "google-cloud-keys.json",
+  projectId: "virtual-dnd-312612"
+});
 const bucketName = "virtual-dnd";
 
 @Injectable()
 export class GameAssetsService {
   uploadAssets({ createReadStream, filename }: FileUpload) {
+    // TODO : Changer le filename et y ajouter un hash aléatoire
+    // ? Si 2 personnes upload une image différente avec un même nom, la 2e image remplacera la première sur GCloud
+
     createReadStream()
       .pipe(
         storage.bucket(bucketName).file(filename).createWriteStream({
@@ -19,11 +25,8 @@ export class GameAssetsService {
         storage
           .bucket(bucketName)
           .file(filename)
-          .makePublic()
-          .then((e) => {
-            console.log(e[0].object);
-            console.log(`https://storage.googleapis.com/${bucketName}/${e[0].object}`);
-          })
+          .get()
+          .then((e) => console.log({ e }))
       );
 
     return true;
